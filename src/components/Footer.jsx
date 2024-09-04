5b0760db3f63a9bf90172b35eebd1fe946707194
np05cp4a220020@iic.edu.np <angilneupane222@gmail.com>
2024-09-04 15:55:37 +0545

    FOOTER Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,17 @@ const Footer = () => {
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="#" className="flex items-center mb-4 sm:mb-0">
+          <a
+            href="#"
+            className="flex flex-col sm:flex-row items-center justify-center text-center mb-4 sm:mb-0"
+          >
             <img
               src="https://i.postimg.cc/nLczy0Vk/ALogo.png"
-              className="h-6"
+              className="h-6 mb-2 sm:mb-0"
               alt="A Logo"
             />
-            <span className="mr-2 text-[25px]">|</span>
-            <span className="self-center text-2xl font-semibold whitespace-nowrap  text-gray-200 dark:text-gray-200">
+            <span className="mr-2 text-[25px] hidden sm:inline">|</span>
+            <span className="text-2xl font-semibold whitespace-nowrap text-gray-800 dark:text-gray-200">
               Mr. Hitman
             </span>
           </a>
@@ -64,7 +67,7 @@ const Footer = () => {
           </ul>
         </div>
         <hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-4" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="block text-sm text-center text-gray-500 dark:text-gray-400">
           © 2024{" "}
           <a href="#" className="hover:underline font-semibold">
             Anjil
